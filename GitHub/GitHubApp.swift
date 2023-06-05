39c75390ed7fcf0fed2551787a2fa9346836f80d
Jamie097 <jakubkylar13@icloud.com>
2023-06-05 15:05:17 +0200

    Initial Commit

---
+++ GitHub/GitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubApp.swift
+//  GitHub
+//
+//  Created by Jakub on 05/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct GitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
